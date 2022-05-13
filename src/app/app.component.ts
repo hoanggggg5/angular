cc78f24658bf6456830c494fc733ba26716bbdc4
Hoang Dang <nghiemdanghoang@gmail.com>
2022-05-13 08:43:20 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,42 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Đây là nơi định nghĩa component và logic
 export class AppComponent {
-  title = 'my-app';
+  // Định nghĩa các biến,
+  // Các biến này có thể nhận được giá trị ở file html
+  title = 'angular we16301 FPT Poly';
+
+  // Khai báo thêm 1 số biến để html sử dụng
+  name = 'tuannda3';
+  class = 'we16301';
+
+  students = [
+    {
+      name: 'tuannda1',
+      id: 'PH1111',
+      status: 0
+    },
+    {
+      name: 'tuannda2',
+      id: 'PH2222',
+      status: 1
+    },
+    {
+      name: 'tuannda3',
+      id: 'PH3333',
+      status: 1
+    }
+  ];
+
+  champs = [
+    {
+      name: 'Lucian',
+      dame: 400,
+      defend: 200,
+      speed: 100,
+      price: 6300,
+      avatar: 'https://oneesports.blob.core.windows.net/cdn-data/sites/4/2021/08/pulsefire-lucian-lien-minh-huyen-thoai1.jpg'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
+import { ChampComponent } from './champ/champ.component';
+import { NameComponent } from './name/name.component';
+import { TableAvatarComponent } from './table/table-avatar/table-avatar.component';
+import { TablePriceComponent } from './table/table-price/table-price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    ChampComponent,
+    NameComponent,
+    TableAvatarComponent,
+    TablePriceComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
